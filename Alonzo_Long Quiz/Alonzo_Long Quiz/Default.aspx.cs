e62c56ac66b5172d147bcf2fd7867758f01ad3a1
Earl Lawrence Alonzo <144116937+Aoru-IT@users.noreply.github.com>
2024-05-19 00:15:18 +0800

    Update

---
+++ Alonzo_Long Quiz/Alonzo_Long Quiz/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace Alonzo_Long_Quiz
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoginStatus1_LoggingOut(object sender, LoginCancelEventArgs e)
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            Response.Redirect("~/Log-In.aspx");
+        }
+    }
+}
